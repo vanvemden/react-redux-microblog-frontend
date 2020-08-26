098e62c7c21adaafe05f81f6f5575674b7f30b00
Marco van Vemden <vanvemden@gmail.com>
2020-08-25 22:10:07 -0700

    remove weclome message

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,31 +1,28 @@
-import React, { useEffect } from 'react'
-import BlogCard from "./BlogCard"
+import React, { useEffect } from "react";
+import BlogCard from "./BlogCard";
 import { useSelector, shallowEqual, useDispatch } from "react-redux";
 import { loadPostsFromApi } from "./actions";
-import { Row, Col } from "reactstrap";
+import { Row } from "reactstrap";
 
 function BlogList() {
-  const { titles } = useSelector((store) => store, shallowEqual);
+  const titles = useSelector((store) => store.titles, shallowEqual);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(loadPostsFromApi())
+    dispatch(loadPostsFromApi());
   }, [dispatch]);
 
   return (
     <div className="BlogList">
       <Row>
-        <Col>
-          <p className="mb-4">Welcome to <b>Microblog</b>, Olivia, Marco and Harry's safe space for expressing your feelings :)</p>
-        </Col>
-      </Row>
-      <Row>
-        {titles.sort((a, b) => b.votes - a.votes).map(post => (
-          <BlogCard post={post} key={post.id} />
-        ))}
+        {titles
+          .sort((a, b) => b.votes - a.votes)
+          .map((post) => (
+            <BlogCard post={post} key={post.id} />
+          ))}
       </Row>
     </div>
-  )
+  );
 }
 
 export default BlogList;
