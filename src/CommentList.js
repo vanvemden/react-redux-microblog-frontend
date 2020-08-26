30ca058f494248e9cfa140c91f59b1e66c0fa365
Marco van Vemden <vanvemden@gmail.com>
2020-08-25 22:11:52 -0700

    remove row component

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,20 +1,16 @@
 import React from "react";
 
 import CommentCard from "./CommentCard";
-import { Row, Container } from "reactstrap";
+import { Container } from "reactstrap";
 
 function CommentList({ postId, comments }) {
-  //  const comments = useSelector(store => store.comments[postId], shallowEqual);
-
   return (
     <div className="CommentList mt-4">
       <h5>Comments</h5>
       {comments ? (
         <Container>
           {comments.map((comment) => (
-            <Row className="my-2">
-              <CommentCard comment={comment} key={comment.id} postId={postId} />
-            </Row>
+            <CommentCard comment={comment} key={comment.id} postId={postId} />
           ))}
         </Container>
       ) : (
